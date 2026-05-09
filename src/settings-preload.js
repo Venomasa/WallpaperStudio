1a4a3466cd323df44940f0f5f45b7d4a67d8c8d7
Venomasa <sniperasa8@gmail.com>
2026-05-09 13:09:27 +0300

    remove old files

--- src/settings-preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-contextBridge.exposeInMainWorld('settingsApi', {
-  getAppSettings:  ()  => ipcRenderer.invoke('get-app-settings'),
-  saveAppSettings: (s) => ipcRenderer.invoke('save-app-settings', s),
-});
--- src/slideshow-preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('slideshow', {
-  start: (ms) => ipcRenderer.send('start-slideshow', ms),
-  stop: () => ipcRenderer.send('stop-slideshow')
-});
